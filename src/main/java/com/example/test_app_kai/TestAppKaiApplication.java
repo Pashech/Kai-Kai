6aef2726ce333ecc0321fdcbe373d0f4cd05ca87
Pashech <Pashech555@gmail.com>
2026-05-04 10:52:10 +0300

    delete

--- src/main/java/com/example/test_app_kai/TestAppKaiApplication.java
+++ src/main/java/com/example/test_app_kai/TestAppKaiApplication.java
@@ -9,12 +9,7 @@ public class TestAppKaiApplication {
 	public static void main(String[] args) {
         SpringApplication.run(TestAppKaiApplication.class, args);
 
-        System.out.println("Тут не должно ничего быть");
-        System.out.println("Тут не должно ничего быть");
-        System.out.println("Тут не должно ничего быть");
-        System.out.println("Тут не должно ничего быть");
-        System.out.println("Тут не должно ничего быть");
-        System.out.println("Тут не должно ничего быть");
+
 
 
     }
